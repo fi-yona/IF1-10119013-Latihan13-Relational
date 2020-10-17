26c99449e082fe1940880d17899a6659fc13ebbb
FionaAvila <fionaavila15@gmail.com>
2020-10-17 18:03:50 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan13/relational/if110119013Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan13.relational;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+
+public class if110119013Latihan13Relational {
+
+    public static void main(String[] args) {
+        
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
